d95f8cdb6bc46967b6a96c1fe980abb7e68925fb
Ken Chow <chow.qed@gmail.com>
2015-04-28 21:11:57 -0500

    new

--- Ahn1995.h
+++ Ahn1995.h
@@ -1,17 +1,20 @@
 #import "DDP"
 
-struct Ahn1995 : ExtremeValue {
-    static decl pi ;  //prob.of boy           // Optional constants (e.g. sigma=6.5;)
 
-	enum{T=25,tau=7};  /** State space dimensions. @names dimens **/
+enum{T=25,tau=7};  /** State space dimensions. @names dimens **/
 
-	static decl d, nc, nb;	  // list action variables, state variables, other things.
-
+struct Ahn : ExtremeValue {
+	
+	static decl d, nb, nbp, EMax;	  // list action variables, state variables, other things.
+	static const decl BoyRatio = 0.499, delt=0.9;   // ratio of births that are boys
+	static decl Noff, p ; // placeholders for variables that maynot be needed
 	static const decl
 		/* estimated parameters from Table 5*/ pars ; 
+ 	static const decl Y=1; // income variables	
 	
-	static Reachable();	    // Static methods can be called anything.
-	
+	static ItsABoy(A);	
+        static Run();	
+	static Reachable();	    // Static methods can be called anything.		
 	static Utility();		  	//Automatic methods
 	static FeasibleActions(Alpha);
 			
